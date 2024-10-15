f460d0b5b014aeac62b40fb82230d388c57f5a53
nandyalapsai <prasannasai.nandyala@motorolasolutions.com>
2024-10-15 10:36:18 +0530

    added gitignore

--- Day_2_141024/Prog1.java
+++ Day_2_141024/Prog1.java
@@ -6,6 +6,6 @@ public class Prog1 {
 
 // DIfferent ways of main method
 // 1.public static void main(String[] args)
-// 2.statuc public void main(String[] args)
+// 2.static public void main(String[] args)
 // 3.public static void main(String args[])
 // 4.public static void main(String... args)
