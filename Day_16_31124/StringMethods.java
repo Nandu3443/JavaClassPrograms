c0f41c5110944186165ed0d06167ebe2c8761f92
nandyalapsai <prasannasai.nandyala@motorolasolutions.com>
2024-11-03 13:14:24 +0530

    Day 16_31124

---
+++ Day_16_31124/StringMethods.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class StringMethods {
+
+	public static void main(String[] args) {
+		String s = "abc";
+		System.out.println("Enter some string:");
+		Scanner sc = new Scanner(System.in);
+//		System.out.println(sc.next());
+//		System.out.println(sc.next());
+//		System.out.println(sc.next());
+//		System.out.println(sc.nextLine());
+		String str = sc.nextLine();
+	}
+
+}
