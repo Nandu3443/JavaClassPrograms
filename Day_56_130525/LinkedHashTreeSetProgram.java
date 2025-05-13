a193568bf037412d7e57887f8464ee1fa7ed84e4
= <=>
2025-05-13 09:47:41 +0530

    Day 56 readded

--- Day_56_130525/LinkedHashTreeSetProgram.java
+++ Day_56_130525/LinkedHashTreeSetProgram.java
@@ -1,4 +1,22 @@
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 public class LinkedHashTreeSetProgram {
-
+	public static void main(String[] args) {
+		LinkedHashSet ls = new LinkedHashSet();
+		TreeSet t = new TreeSet();
+		String str = "Mississippi";
+		char ar[] = str.toCharArray();
+		for(int i=0;i<ar.length;i++) {
+			ls.add(ar[i]);
+			t.add(ar[i]);
+		}
+		for(Object o:ls) {
+			System.out.print(o);
+		}
+		System.out.println();
+		for(Object o:t) {
+			System.out.print(o);
+		}
+	}
 }
